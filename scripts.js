f9267c355d8940b224288abaf093aabe4e69001f
Mongfer <devastate450@gmail.com>
2023-09-19 22:19:16 +0700

    add limit addTask function

--- scripts.js
+++ scripts.js
@@ -9,30 +9,45 @@ let addTask = function () {
     alert("Enter your task"); // if no text in input box, it will alert
   } else {
     let li = document.createElement("li"); // create element(li) store in var(li)
-    li.innerHTML = task; // create li and print on page with inputBox.value 
-    taskList.appendChild(li); // Append li inside of TaskList 
-    inputBox.value = "";  // reset inputBox
+    li.innerHTML = task; // create li and print on page with inputBox.value
+    taskList.appendChild(li); // Append li inside of TaskList
+    inputBox.value = ""; // reset inputBox
     let span = document.createElement(`span`); // create element(span) store in var(span)
-    span.innerHTML = "❌"; // create span and print on page with ❌ 
-    li.appendChild(span); // Append span right-side of li 
+    span.innerHTML = "❌"; // create span and print on page with ❌
+    li.appendChild(span); // Append span right-side of li
   }
 };
 
-// Click to addTask
-addBtn.addEventListener("click", function () {
-  addTask(); // if user click #addBtn addTask() will run 
-}); 
+// limit addTask
+let addNum = 0;
 
-// Press Enter to addTask
-inputBox.addEventListener(`keyup`, function (e) {
-  if (e.key === "Enter") addTask(); // if user press "Enter" addTask() will run
-}); 
+function addCount() {
+  const addLimit = 8;
+  if (addNum <= addLimit) {
+    addTask(); // add Task function from line 5
+    addNum++; // addNum value +1
+    console.log(addNum);
+  } else if (addNum > addLimit) {
+    alert("Please remove some tasks");
+}
+}
 
 // remove&cross out function
 taskList.addEventListener("click", function (e) {
-  if (e.target.tagName === "SPAN") { // if element tagname <span>
+  if (e.target.tagName === "SPAN") {
+    // if element tagname <span>
     e.target.parentElement.remove(); // parentElement of <span> will be remove and <span> too
-  } else if (e.target.tagName === "LI") { // if element tagname <li>
-    e.target.classList.toggle("checked");// it will toggle class = "checked"
+    addNum--;
+    console.log(addNum);
+  } else if (e.target.tagName === "LI") {
+    // if element tagname <li>
+    e.target.classList.toggle("checked"); // it will toggle class = "checked"
   }
+});
+
+addBtn.addEventListener("click", addCount);
+
+// Press Enter to addTask
+inputBox.addEventListener(`keyup`, function (e) {
+  if (e.key === "Enter") addCount(); // if user press "Enter" addTask() will run
 });
